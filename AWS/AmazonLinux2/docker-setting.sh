75e65dbd4840f5e0ce562619b3e13f3ba9400a98
Kento Takano <kento2github@gmail.com>
2020-08-08 14:25:37 +0900

    Added docker settings

---
+++ AWS/AmazonLinux2/docker-setting.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+HOST_NAME="STANDARD-SETTING"
+
+### Amazon Linux 2
+
+# Package Update
+sudo yum update -y
+
+# timezon
+echo "Set TimeZone"
+sudo timedatectl set-timezone Asia/Tokyo
+
+# hostname
+echo "Set HostName : $HOST_NAME"
+sudo hostnamectl set-hostname --static "$HOST_NAME"
+sudo echo 'preserve_hostname: true' >> /etc/cloud/cloud.cfg
+
+# locale
+echo "default lunguage is ja_JP.utf8"
+sudo localectl set-locale LANG=ja_JP.utf8
+
+# time sync
+echo "Settings chrony"
+sudo yum erase -y 'ntp*'
+sudo yum install -y chrony
+sudo echo '#Add TimeSync' >> /etc/chrony.conf
+sudo echo 'server 169.254.169.123 prefer iburst' >> /etc/chrony.conf
+sudo systemctl start chrony
+sudo systemctl enable chrony
+
+# docker
+sudo amazon-linux-extras install -y docker
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo gpasswd -a ec2-user docker
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# end
+
