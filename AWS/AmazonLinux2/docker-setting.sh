66564e2533320cd34949d523b6bbfc9de89d0c7b
Kento Takano <kento2github@gmail.com>
2020-08-08 14:33:21 +0900

    Update docker-setting.sh

--- AWS/AmazonLinux2/docker-setting.sh
+++ AWS/AmazonLinux2/docker-setting.sh
@@ -33,7 +33,7 @@ sudo systemctl enable chrony
 sudo amazon-linux-extras install -y docker
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo gpasswd -a ec2-user docker
+sudo usermod -a -G docker ec2-user
 
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
